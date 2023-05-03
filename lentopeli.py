70d56dcf34e4ec6a4ca52c8567c24417a9dc4396
Suzu10 <petra.haukkala@gmail.com>
2023-05-03 16:52:31 +0300

    3/5/2023 16:02 Maavalinnat

--- lentopeli.py
+++ lentopeli.py
@@ -48,15 +48,60 @@ class Tietokanta:
             print(names)
         return names
 
+    def random_country(self):
+        potential_countries = []
+        country_check = []
+        for v_country in visited_countries:
+            country_check.append(v_country)
+        kerrat = 0
+        while kerrat < 3:
+            sql = "select Nimi from maat ORDER BY RAND() LIMIT 1"
+            cursor = self.cnx.cursor()
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            """print(result)"""
+            for n in result:
+                country = n[0]
+                if country not in country_check:
+                    print(country)
+                    print(country_check)
+                    if country not in potential_countries:
+                        potential_countries.append(country)
+                        kerrat = kerrat + 1
+                    else:
+                        print('Mukana Potential: ' + country)
+                else:
+                    print('Mukana Visited: ' + country)
+        return potential_countries
+
 @app.route('/countryoptions')
 def get_country_options():
-    connection = Tietokanta('localhost', 3306, 'flight_game', 'root', 'm!näk00d44n')
+    connection = Tietokanta('localhost', 3306, 'flight_game', 'root', 'Suzu')
     connection.connect()
     country_options = connection.get_country_options()
     print(country_options)
 
     return Response(response=json.dumps(country_options, ensure_ascii=False).encode('utf8'),
                     status=200, mimetype="application/json")
+
+@app.route('/randomcountry')
+def get_random_country_list():
+    connection = Tietokanta('localhost', 3306, 'flight_game', 'root', 'Suzu')
+    connection.connect()
+    country_list = connection.random_country()
+    print(country_list)
+
+    return Response(response=json.dumps(country_list, ensure_ascii=False).encode('utf8')
+                    , status=200, mimetype="application/json")
+
+@app.route('/add_selected_country/<current_country>')
+def add_selected_country(current_country):
+    print("Python saa nykyisen maan: " + current_country)
+    visited_countries.append(current_country)
+    """print(visited_countries)"""
+
+    return Response(response=json.dumps(visited_countries, ensure_ascii=False).encode('utf8')
+                    , status=200, mimetype="application/json")
 
 @app.route('/top5')
 def get_top5_list():
@@ -68,6 +113,7 @@ def get_top5_list():
     return Response(response=json.dumps(top5, ensure_ascii=False).encode('utf8'),
                     status=200, mimetype="application/json")
 
+visited_countries = []
 
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=3000)
